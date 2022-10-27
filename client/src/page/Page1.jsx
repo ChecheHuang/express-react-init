f8e391eba6c9f7789d115850536bee9c3256a017
Carl <carlhuang@sunnygo.com.tw>
2022-10-27 14:15:10 +0800

    modify

--- client/src/page/Page1.jsx
+++ client/src/page/Page1.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { updateUser } from '../redux/apiCalls';
+import { sampleApi } from '../utils/APIRoutes';
 
 function Page1() {
   const { userInfo, pending, error } = useSelector((state) => state.user);
   const dispatch = useDispatch();
   useEffect(() => {
     console.log('userInfo', userInfo);
+    console.log('sampleApi', sampleApi);
     updateUser('url', { name: 'carl', email: 'email@gmial.com.tw' }, dispatch);
   }, []);
   return (
